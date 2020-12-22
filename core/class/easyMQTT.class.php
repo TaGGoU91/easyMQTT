f4dbf168a1f50697227e6ac14bd69ef4c8aa42b2
TaGGoU91 <30494278+TaGGoU91@users.noreply.github.com>
2020-12-22 11:42:41 +0100

    typo

--- core/class/easyMQTT.class.php
+++ core/class/easyMQTT.class.php
@@ -332,7 +332,7 @@ class easyMQTT extends eqLogic {
 						}						
 					// utiliser isset(exposes['features']) pour remplacer le array_key_exists 
 						if (isset($exposes['features'])) {
-							log::add('easyMQTT', 'debug', '---------------------- La clef features a été trouvé dans le tableau exposes, donc on traite différement les données');
+							log::add('easyMQTT', 'debug', '---------------------- La clef features a été trouvée dans le tableau exposes, donc on traite différement les données');
 							foreach($value['definition']['exposes']['features'] as $feature){
 								log::add('easyMQTT', 'debug', 'Valeur de [definition][exposes][features][access]  : '. $feature['access'] .'');
 								log::add('easyMQTT', 'debug', 'Valeur de [definition][exposes][features][name] : '. $feature['name'] .'');
